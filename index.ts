947fd55b842ae977f8ee8f635fa4baed09fd99c2
Hassen Dhaouadi <Dhaouadi002@gmail.com>
2024-04-27 00:22:21 +0300

    package .json + index.ts

--- index.ts
+++ index.ts
@@ -1,24 +1,2 @@
-import { generateOtp } from "./src/module/otp";
-import { generatePassword } from "./src/module/password";
-
-console.log(
-  generatePassword({
-    length: 2,
-    chars: {
-      useDigits: true,
-      useLowercase: true,
-      useSymbols: true,
-      useUppercase: true,
-    },
-  })
-);
-
-console.log(
-  generateOtp({
-    length: 12,
-    chars: {
-      useDigits: true,
-      useUppercase:true,
-    },
-  })
-);
+export * from './src/module/otp'
+export * from './src/module/password'
